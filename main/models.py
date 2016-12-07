9f5088a21e2653b750a1a348ad10644b36f694a2
M4voTest <m4vo2@interia.pl>
2016-12-07 16:56:22 +0100

    Commit
    Ustawiono url, dodano modele, admina, superusera, template wersja
    testowa

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    article_title = models.CharField(max_length=200)
+    article_text = models.TextField()
+    article_author = models.CharField(max_length=200)
+    article_pub_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.article_title
--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views import generic
+from . import models
 # Create your views here.
+
+
+class IndexView(generic.ListView):
+    template_name = 'blog/IndexView.html'
+    model = models.Article
